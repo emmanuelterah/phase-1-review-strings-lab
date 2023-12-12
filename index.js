26d965954ff64766e99345e714e4764f8bb42e81
emmanuelterah <emmanuelterah98@gmail.com>
2023-12-12 17:08:07 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code in this file!
+
+let currentUser = "Grace Hopper"
+//////
+
+let welcomeMessage = "Welcome to Flatbook, " + currentUser + "!"
+
+///////
+
+let upperCaseCurrentUser = "GRACE HOPPER"
+
+let excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + upperCaseCurrentUser + "!"
+
+///////
+
+let shortGreeting = "Welcome, " + currentUser[0] + "!"
